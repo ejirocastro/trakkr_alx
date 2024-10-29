0cc248a02802341b8fe7650621a1a63a94fd4e64
ejirocastro <awenayericastrop@gmail.com>
2024-10-29 04:03:54 +0100

    fixed a bug in routes.js file

--- Server/Startups/routes.js
+++ Server/Startups/routes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import userRouter from "../Routes/userRoutes";
+import userRouter from "../Routes/userRoutes.js";
 
-export const mountedRoutes = function (app) {
+export const mountedRoutes = function (app)
+{
   app.use(express.json());
   app.use("/api/v1/users", userRouter);
 };
