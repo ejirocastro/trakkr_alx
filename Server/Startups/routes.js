6d1aa2a6c5a40578f38771e5d757eeb3e8618953
ejirocastro <awenayericastrop@gmail.com>
2024-10-31 15:09:31 +0100

    Set up routes.js to define application routing and endpoints

--- Server/Startups/routes.js
+++ Server/Startups/routes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import userRouter from "../Routes/userRoutes.js";
 
+// Function to mount routes on the Express app
 export const mountedRoutes = function (app)
 {
+  // Middleware to parse incoming JSON requests
   app.use(express.json());
+
+  // Mount the userRouter on the "/api/v1/users" path
+  // Any route beginning with "/api/v1/users" will be handled by userRouter
   app.use("/api/v1/users", userRouter);
 };
