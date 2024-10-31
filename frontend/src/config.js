2f3632079304fb945d5d97e83bcc26e79f4c06e1
ejirocastro <awenayericastrop@gmail.com>
2024-10-31 13:56:33 +0100

    Establish config.js for application-wide configuration settings

---
+++ frontend/src/config.js
@@ -0,0 +1,4 @@
+// export const BASE_URL = "https://tourapp-t1z3.onrender.com/api/v1";
+export const BASE_URL = "http://localhost:5000/api/v1";
+export const token = localStorage.getItem("token");
+export const API_KEY = "at_5YyF8eyT9jTVILm81B6VJqkwgRi5P";
