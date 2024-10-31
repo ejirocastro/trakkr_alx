5d9ed695be31ae48fe3d55e8ad5037fe323abc28
ejirocastro <awenayericastrop@gmail.com>
2024-10-31 15:06:26 +0100

    Implement GenericService.js for centralized API interaction

--- Server/services/GenericService.js
+++ Server/services/GenericService.js
@@ -1,6 +1,5 @@
 import catchAsync from "../utils/catchAsync.js";
 import AppError from "../errorHandlers/appError.js";
-import APIFeatures from "../utils/apiFeatures.js";
 
 export const deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -47,11 +46,9 @@ export const createOne = (Model) =>
     });
   });
 
-export const getOne = (Model, popOptions) =>
+export const getOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    let query = Model.findById(req.params.id);
-    if (popOptions) query = query.populate(popOptions);
-    const doc = await query;
+    const doc = Model.findById(req.params.id);;
 
     if (!doc) {
       return next(new AppError("No document found with that ID", 404));
@@ -67,17 +64,8 @@ export const getOne = (Model, popOptions) =>
 
 export const getAll = (Model) =>
   catchAsync(async (req, res, next) => {
-    // To allow for nested GET reviews on tour (hack)
-    let filter = {};
-    if (req.params.tourId) filter = { tour: req.params.tourId };
 
-    const features = new APIFeatures(Model.find(filter), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    // const doc = await features.query.explain();
-    const doc = await features.query;
+    const doc = await Model.find();
 
     // SEND RESPONSE
     res.status(200).json({
