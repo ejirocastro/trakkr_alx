5f35f602a4f414c17edb3f28c28ebc00ffe49fd2
ejirocastro <awenayericastrop@gmail.com>
2024-10-31 13:54:06 +0100

    Initialize main.jsx as the root of the React application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext.jsx'
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+    <AuthContextProvider>
+        <ToastContainer
+          theme="light"
+          position='top-center'
+          autoClose={5000}
+          closeOnClick={true}
+          pauseOnFocusLoss={false}
+          pauseOnHover={false}
+          toastClassName=".toast-message"
+        />
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
