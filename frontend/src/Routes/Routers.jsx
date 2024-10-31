a1b9235fa2e2f13f12f2fa89b8c93fbb09cc9679
ejirocastro <awenayericastrop@gmail.com>
2024-10-31 02:43:28 +0100

    Add structured routing setup for application with authentication and user account pages

--- frontend/src/Routes/Routers.jsx
+++ frontend/src/Routes/Routers.jsx
@@ -2,6 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import Landing from "../pages/Landing";
 import Login from '../components/Auth/Login';
 import SignUp from '../components/Auth/Signup';
+import EmailVerificationSent from '../pages/EmailVerificationSent';
+import EmailVerified from '../pages/EmailVerified';
+import ResetPassword from '../pages/ResetPassword';
+import PasswordForgot from '../pages/PasswordForgot';
+import Dashboard from '../pages/Dashboard';
 
 
 
@@ -10,8 +15,13 @@ const Routers = () =>
     return <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/verify" element={<EmailVerificationSent />} />
+        <Route path="/verified" element={<EmailVerified />} />
+        <Route path="/forgotpassword" element={<PasswordForgot />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
     </Routes>
 };
 
