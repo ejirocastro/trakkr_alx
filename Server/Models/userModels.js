7d18cc582fe20aa133e26833270844c0b5c61f3b
ejirocastro <awenayericastrop@gmail.com>
2024-10-31 15:08:17 +0100

    Create userModels.js to define user-related data models

--- Server/Models/userModels.js
+++ Server/Models/userModels.js
@@ -69,9 +69,6 @@ userSchema.pre("save", function (next)
 
 userSchema.pre(/^find/, function (next)
 {
-  //this points to the current query
-  // if (this.getOptons().role == "admin") return next();
-
   this.find({ active: { $ne: false } });
   next();
 });
